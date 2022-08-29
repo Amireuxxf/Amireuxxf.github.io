a40865b6eceea757ce4577ddbb7d4374295a2521
赵启航 <1776331612@qq.com>
2022-08-29 19:31:12 +0800

    Site updated: 2022-08-29 19:31:10

--- js/runtime/runtime.js
+++ js/runtime/runtime.js
@@ -1,7 +1,7 @@
 var now = new Date;
 
 function createtime() {
-    var grt = new Date("04/01/2021 00:00:00");
+    var grt = new Date("08/20/2022 16:00:00");
     now.setTime(now.getTime() + 250);
     var days = (now - grt) / 1e3 / 60 / 60 / 24, dnum = Math.floor(days),
         hours = (now - grt) / 1e3 / 60 / 60 - 24 * dnum, hnum = Math.floor(hours);
@@ -11,7 +11,7 @@ function createtime() {
     var seconds = (now - grt) / 1e3 - 86400 * dnum - 3600 * hnum - 60 * mnum, snum = Math.round(seconds);
     1 == String(snum).length && (snum = "0" + snum);
     let currentTimeHtml = "";
-    currentTimeHtml = hnum < 18 && hnum >= 9 ? `<img class='boardsign' src='https://cdn.cbd.int/anzhiyu-blog@2.0.3/img/badge/安知鱼-上班摸鱼中.svg' title='距离月入25k也就还差一个大佬带我~'><span class='textTip'> <br> 本站居然运行了 ${dnum} 天</span><span id='runtime'> ${hnum} 小时 ${mnum} 分 ${snum} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>` : `<img class='boardsign' src='https://cdn.cbd.int/anzhiyu-blog@2.0.3/img/badge/安知鱼-下班啦.svg' title='下班了就该开开心心的玩耍，嘿嘿~'><span class='textTip'> <br> 本站居然运行了 ${dnum} 天</span><span id='runtime'> ${hnum} 小时 ${mnum} 分 ${snum} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`, document.getElementById("workboard") && (document.getElementById("workboard").innerHTML = currentTimeHtml)
+    currentTimeHtml = hnum < 18 && hnum >= 9 ? `<img class='boardsign' src='https://cdn.cbd.int/anzhiyu-blog@2.0.3/img/badge/赵小航-上班摸鱼中.svg' title='距离月入25k也就还差一个大佬带我~'><span class='textTip'> <br> 本站居然运行了 ${dnum} 天</span><span id='runtime'> ${hnum} 小时 ${mnum} 分 ${snum} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>` : `<img class='boardsign' src='https://cdn.cbd.int/anzhiyu-blog@2.0.3/img/badge/赵小航-下班啦.svg' title='下班了就该开开心心的玩耍，嘿嘿~'><span class='textTip'> <br> 本站居然运行了 ${dnum} 天</span><span id='runtime'> ${hnum} 小时 ${mnum} 分 ${snum} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`, document.getElementById("workboard") && (document.getElementById("workboard").innerHTML = currentTimeHtml)
 }
 
 setInterval(() => {
