693b26d8aa5e2b145e07fbc592ff22377847da8d
赵启航 <1776331612@qq.com>
2022-08-29 19:50:17 +0800

    Site updated: 2022-08-29 19:50:15

--- js/runtime/runtime.js
+++ js/runtime/runtime.js
@@ -1,7 +1,7 @@
 var now = new Date;
 
 function createtime() {
-    var grt = new Date("04/01/2021 00:00:00");
+    var grt = new Date("08/20/2022 00:00:00");
     now.setTime(now.getTime() + 250);
     var days = (now - grt) / 1e3 / 60 / 60 / 24, dnum = Math.floor(days),
         hours = (now - grt) / 1e3 / 60 / 60 - 24 * dnum, hnum = Math.floor(hours);
