09afa646ff636b78e9783cb115f850bf6cf0b392
赵启航 <1776331612@qq.com>
2022-10-08 18:56:06 +0800

    Site updated: 2022-10-08 18:56:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/7b72b0de/","posts/b7e07608/","posts/4a098468/","posts/4a17b156/","posts/9b28f329/","posts/7d637b24/","posts/ddfda2a8/","posts/4dbbe0bc/","posts/a60727c/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/7b72b0de/","posts/b7e07608/","posts/9b28f329/","posts/4a098468/","posts/7d637b24/","posts/4a17b156/","posts/4dbbe0bc/","posts/ddfda2a8/","posts/a60727c/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
